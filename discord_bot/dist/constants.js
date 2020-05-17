c7766f53d74bbd9dc2757afbdf43de66b5195b23
Patrik Vácal <patrik.vacal@gmail.com>
2020-05-17 08:04:07 +0200

    Added some testing files, removed dist build from discord bot.

--- discord_bot/dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BOT_JOIN_URL = exports.PORT = void 0;
-exports.PORT = 3001;
-exports.BOT_JOIN_URL = 'https://discord.com/api/oauth2/authorize?client_id=711344691429244929&permissions=2147483639&scope=bot';
-//# sourceMappingURL=constants.js.map
